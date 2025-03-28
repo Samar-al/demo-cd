4607d324c5ea19605bfe93cc8c2e9c6e7a864531
Samar-al <samar.alkhalilchebaye@gmail.com>
2025-03-28 14:47:34 +0100

    fix

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("oui ça marche !");
+const button = document.getElementById('myButton');
+const output = document.getElementById('output');
+
+if (button && output) {
+  button.addEventListener('click', () => {
+    output.textContent = 'Tu as cliqué !';
+  });
+}
+
+console.log("oui ça marche !");
+
